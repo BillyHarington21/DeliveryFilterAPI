92f9dcdc320cd90f5e2ef01cd8c957e327c5cbae
CENTURION 1 <kdtkachenko@gmail.com>
2024-10-22 01:28:48 +0300

    добавляем основные сущности и интерфейс с основными методами для работы с заказами

---
+++ Domen/Repository/IOrderRepository.cs
@@ -0,0 +1,16 @@
+﻿using Domen.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domen.Repository
+{
+    public interface IOrderRepository
+    {
+        Task<List<Order>> GerAllOrders();
+        Task<Order?> GerOrderById(Order order);
+        Task AddOrder(Order order);
+    }
+}
